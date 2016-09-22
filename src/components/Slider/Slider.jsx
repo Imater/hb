a801737a87d96495f513bcf862f480939a16bab8
Imater <eugene.leonar@gmail.com>
2016-09-22 11:16:47 +0500

    add shake slider on home and end

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -4,23 +4,29 @@ import styles from './Slider.styl'
 
 class Slider extends PureComponent {
 
+  state = {
+    offset: 0
+  }
+
   static propTypes = {
     slides: pt.arrayOf(pt.node),
     current: pt.number,
     onChange: pt.func,
     direction: pt.oneOf(['vertical', 'horizontal']),
-    isKeyActive: pt.bool
+    isKeyActive: pt.bool,
+    isLoop: pt.bool
   }
 
   static defaultProps = {
     slides: [],
     direction: 'horizontal',
-    isKeyActive: true
+    isKeyActive: true,
+    isLoop: false
   }
 
   generateNumberKeysFn = initKeys => {
     const keys = {
-      'Digit0': this.setCurrent.bind(this, 0),
+      Digit0: this.setCurrent.bind(this, 0),
       ...initKeys
     }
     for (let index = 1; index <= 9; index += 1) {
@@ -34,6 +40,10 @@ class Slider extends PureComponent {
       ArrowLeft: this.handleLeftClick,
       ArrowUp: this.handleLeftClick,
       PageUp: this.handleLeftClick,
+      KeyH: this.handleLeftClick,
+      KeyK: this.handleLeftClick,
+      KeyJ: this.handleRightClick,
+      KeyL: this.handleRightClick,
       Space: this.handleRightClick,
       ArrowRight: this.handleRightClick,
       ArrowDown: this.handleRightClick,
@@ -70,10 +80,23 @@ class Slider extends PureComponent {
     </div>
   )
 
+  shake = index => {
+    this.setState({
+      offset: index < 0 ? -30 : 30
+    })
+    setTimeout(() => {
+      this.setState({
+        offset: 0
+      })
+    }, 200)
+  }
+
   setCurrent = index => {
     const { onChange, slides } = this.props
     if (index >= 0 && index < slides.length) {
       onChange(index)
+    } else {
+      this.shake(index)
     }
   }
 
@@ -87,20 +110,21 @@ class Slider extends PureComponent {
   }
 
   handleLeftClick = () => {
-    const { current, slides } = this.props
-    const nextCurrent = current === 0 ? slides.length - 1 : current - 1
+    const { current, slides, isLoop } = this.props
+    const nextCurrent = current === 0 && isLoop ? slides.length - 1 : current - 1
     this.setCurrent(nextCurrent)
   }
 
   handleRightClick = () => {
-    const { current, slides } = this.props
-    const nextCurrent = slides.length - 1 === current ? 0 : current + 1
+    const { current, slides, isLoop } = this.props
+    const nextCurrent = slides.length - 1 === current && isLoop ? 0 : current + 1
     this.setCurrent(nextCurrent)
   }
 
   render() {
     const { current, slides, direction } = this.props
-    const marginLeft = -(current * 100)
+    const { offset } = this.state
+    const marginLeft = -(current * 100 + offset)
     const translateDirection = direction === 'horizontal' ? 'translateX' : 'translateY'
     return (
       <div className={styles.Slider}>
